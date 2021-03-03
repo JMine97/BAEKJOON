3cb45a04433407446a6d2b8cca33524b252a57d2
JMine97 <52027965+JMine97@users.noreply.github.com>
2021-03-03 16:24:52 +0900

    Add files via upload

---
+++ Floyd-Warshall/2458.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+n, m = map(int, input().split())
+# 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용은 C라고 설정
+    a, b = map(int, input().split())
+    c=1
+    graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            if graph[a][k] and graph[k][b]:
+                graph[a][b]=1
+
+result=0
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    cnt=0
+    for b in range(1, n + 1):
+        if graph[a][b] or graph[b][a]:
+            cnt+=1
+
+    if cnt==n-1:
+        result+=1
+
+print(result)
